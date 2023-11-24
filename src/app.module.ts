24907e4706210cb6d66e817b68824518d2223065
DIMITA <dimitriomorales01@gmail.com>
2023-11-24 21:55:48 +0100

    Config de l'application (AMQP, typeorm)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,45 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ClientRMQ, ClientsModule, Transport } from '@nestjs/microservices';
+import { Users } from './users/entities/user.entity';
+
+const user = 'guest';
+const pwd = 'guest';
+const host = 'rabbitmq';
+const port = 5672;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: `${__dirname}/data/db.sqlite`,
-      entities: [],
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'rootpw',
+      database: 'subscription',
       synchronize: true,
+      entities: ['dist/**/*.entity.js'],
+      migrationsTableName: 'migration',
+      migrations: ['src/migration/*.ts'],
+      logging: true,
     }),
+    TypeOrmModule.forFeature([Users]),
+    ClientsModule.register([
+      {
+        name: 'MAIL_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [`amqp://${user}:${pwd}@${host}:${port}`],
+          queue: 'sendmail_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ClientRMQ],
 })
 export class AppModule {}
