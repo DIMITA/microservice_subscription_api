85b632317efcac1a2e0f26ff1a2fd661696114b5
DIMITA <dimitriomorales01@gmail.com>
2023-11-24 09:26:58 +0100

    initialisation de l'application nest et ajout de la connection à RMQ

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+const user = 'guest';
+const pwd = 'guest';
+const host = 'localhost';
+const port = 5672;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${pwd}@${host}:${port}`],
+      queue: 'sendmail_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await app.listen(3009);
 }
 bootstrap();
